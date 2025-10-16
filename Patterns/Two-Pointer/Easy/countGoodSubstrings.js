bcaa135fe1957429294d65a07f86f592b88aa304
kabilanP4 <kabilanperumal6@gmail.com>
2025-10-16 07:41:19 +0530

    Leetcode #3,#1876, sliding window problems

---
+++ Patterns/Two-Pointer/Easy/countGoodSubstrings.js
@@ -0,0 +1,15 @@
+function countGoodSubstrings(s) {
+  let count = 0;
+  for (let i = 0; i < s.length - 2; i++) {
+    let a = s[i],
+      b = s[i + 1],
+      c = s[i + 2];
+    if (a !== b && b !== c && c !== a) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const count = countGoodSubstrings("aababcabc");
+console.log("count", count);
---
+++ Patterns/Two-Pointer/Medium/lengthOfLongestSubstring.js
@@ -0,0 +1,17 @@
+//Sliding window variable size pattern
+function lengthOfLongestSubstring(s) {
+  let start = 0;
+  let maxLength = -Infinity;
+  let set = new Set();
+  for (let end = 0; end < s.length; end++) {
+    while (set.has(s[end])) {
+      set.delete(s[start]); // resetting the set
+      start++;
+    }
+    set.add(s[end]);
+    maxLength = Math.max(maxLength, set.size);
+  }
+  return maxLength === -Infinity ? 0 : maxLength;
+}
+const length = lengthOfLongestSubstring("pwwkew");
+console.log("longest substring length", length);
