3d8da8b3f2a225ed75f4a8182596aff272d8794c
Kabilanp4 <kabilanperumal6@gmail.com>
2026-01-08 09:55:05 +0530

    Optimized version

--- TopInterview150/Array_String/hIndex#274.js
+++ TopInterview150/Array_String/hIndex#274.js
@@ -4,25 +4,15 @@
  */
 var hIndex = function (citations) {
   citations.sort((a, b) => a - b);
-  let start = 0;
-  function count(val) {
-    for (let j = start; j < citations.length; j++) {
-      if (citations[j] >= val) {
-        return citations.length - j;
-      }
-      //return 0;
-    }
-    return count;
-  }
   let max = 0;
   for (let i = 0; i < citations.length; i++) {
-    if (count(i + 1) >= i + 1) {
+    if (citations[citations.length - (i + 1)] >= i + 1) {
       max = Math.max(max, i + 1);
     }
   }
   return max;
 };
-const result = hIndex([0]);
+const result = hIndex([1, 3, 1]);
 console.log("result", result);
 
-//Need to optimize the code tomorrow//
+//Need to optimize the code tomorrow// - Optimized
