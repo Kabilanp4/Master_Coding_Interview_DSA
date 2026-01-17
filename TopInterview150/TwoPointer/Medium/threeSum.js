adcd5ab5b93e6d04d2fa64f06675ba5705cd8044
Kabilanp4 <kabilanperumal6@gmail.com>
2026-01-17 13:46:11 +0530

    #Leetcode #15

---
+++ TopInterview150/TwoPointer/Medium/threeSum.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  if (!nums.length) return nums;
+  nums.sort((a, b) => a - b);
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (i !== 0 && nums[i - 1] === nums[i]) continue;
+    let right = nums.length - 1;
+    let left = i + 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) {
+          left++;
+        }
+        while (left < right && nums[right] === nums[right - 1]) {
+          right--;
+        }
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return result;
+};
+const output = threeSum([-1, 0, 1, 2, -1, -4]);
+console.log("output", output);
