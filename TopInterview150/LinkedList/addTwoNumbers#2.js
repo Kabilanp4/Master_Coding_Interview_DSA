6bd36a156c611fc46bfedfa85c13814efa3b166b
Kabilanp4 <kabilanperumal6@gmail.com>
2026-01-24 15:23:18 +0530

    Leetcode #2, Lesson learned : Sometime we never know how close we are, so don't just give up. Trust your instincts.

--- TopInterview150/LinkedList/addTwoNumbers#2.js
+++ TopInterview150/LinkedList/addTwoNumbers#2.js
@@ -10,31 +10,67 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {
-  let l1Str = "";
-  let l2Str = "";
-  while (l1) {
-    l1Str += String(l1.val);
-    l1 = l1.next;
-  }
-  while (l2) {
-    l2Str += String(l2.val);
-    l2 = l2.next;
-  }
+// Brute force and the limitations are there, not optimal.
+// var addTwoNumbers = function (l1, l2) {
+//   let l1Arr = [];
+//   let l2Arr = [];
+//   while (l1) {
+//     l1Arr.push(l1.val);
+//     l1 = l1.next;
+//   } // O(n)
+//   while (l2) {
+//     l2Arr.push(l2.val);
+//     l2 = l2.next;
+//   } //O(n)
 
-  const result = String(Number(l1Str).reverse() + Number(l2Str).reverse());
-  function buildList(len) {
-    //base case
-    const length = len - 1;
-    const value = result[length];
-    if (!value) return null;
+//   const result =
+//     Number(l1Arr.reverse().join("")) + Number(l2Arr.reverse().join(""));
+//   const resultStr = String(result);
+//   console.log("resultStr", resultStr);
+//   function buildList(len) {
+//     //base case
+//     const length = len - 1;
+//     const value = resultStr[length];
+//     if (!value) return null;
+//     let node = {
+//       val: Number(value),
+//       next: null,
+//     };
+//     node.next = buildList(length);
+//     return node;
+//   }
+//   const finalList = buildList(resultStr.length);
+//   return finalList;
+// };
+
+// Optimal code,
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  function add(l1, l2, carry) {
+    if (!l1 && !l2 && carry === 0) return null;
+    let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+    let mod = sum % 10; //0
+    carry = Math.floor(sum / 10);
     let node = {
-      val: Number(value),
+      val: mod,
       next: null,
     };
-    node.next = buildList(length);
+    let first = l1 ? l1.next : null;
+    let second = l2 ? l2.next : null;
+    node.next = add(first, second, carry);
     return node;
   }
-  const finalList = buildList(result.length);
-  return finalList;
+  const list = add(l1, l2, 0);
+  return list;
 };
