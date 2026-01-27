9fef823cd63f9f4794412fe98e996af1fc443652
Kabilanp4 <kabilanperumal6@gmail.com>
2026-01-27 23:21:13 +0530

    Never traverse a linked list thorugh an indices, interesting solution

--- TopInterview150/LinkedList/rotateRight#61.js
+++ TopInterview150/LinkedList/rotateRight#61.js
@@ -1,3 +1,5 @@
+// Working code but zero readability and maintanability.
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -12,70 +14,94 @@
  */
 var rotateRight = function (head, k) {
   // Edge case
-  if (!head || !head.next) return head;
-  let totalLen = 1;
-  function reverse(list) {
+  if (!head || !head.next || !k) return head;
+  function reverse(start, end, list, sec) {
     let first = list;
     let second = first.next;
     let tail = list;
-    while (second) {
-      let third = second.next;
-      second.next = first;
-      first = second;
-      second = third;
-      totalLen++;
-    }
-    tail.next = null;
-    return first;
-  }
-
-  function reverseStart(list) {
-    let first = list;
-    let second = first.next;
-    let tail = list;
-    let i = 1;
-    while (second && i < k % totalLen) {
+    let i = start;
+    while (second && i < end) {
       let third = second.next;
       second.next = first;
       first = second;
       second = third;
       i++;
     }
-    tail.next = second;
+    if (sec === "all") {
+      tail.next = null;
+    }
+    if (sec === "start") {
+      tail.next = second;
+    }
     return first;
   }
-
-  function reverseEnd(list) {
+  function reverseEnd(start, end, list) {
     let slow = list;
-    let itr = k % totalLen;
-    while (itr - 1) {
+    let itr = start - 1;
+    while (itr && slow) {
       slow = slow.next;
       itr--;
     }
-    console.log("slow", slow);
-    let first = slow;
-    let second = first.next;
-    let end = slow;
-    while (second) {
-      let third = second.next;
-      second.next = first;
-      first = second;
-      second = third;
+    if (slow && slow.next) {
+      let reversed = reverse(start, end, slow.next, "all");
+      slow.next = reversed;
     }
-    end.next = first;
     return list;
   }
-  let reversedList = reverse(head);
-  let reversedStart = reverseStart(reversedList);
-  let reversedEnd = reverseEnd(reversedStart);
-  console.log("reversedList", reversedList);
-  console.log("reversedStart", reversedStart);
-  console.log("reversedEnd", reversedEnd);
-  console.log("totalLen", totalLen);
 
-  // let reversedStart = reverseStart(1,reversedList);
-  // let reversedEnd = reverseEnd(reversedStart);
-  // console.log("reversedEnd",reversedEnd);
-  // return reversedEnd;
-  // return reversedEnd;
+  let totalLen = 0;
+  let curr = head;
+  while (curr) {
+    curr = curr.next;
+    totalLen++;
+  }
+  let rotateCount = k % totalLen;
+  if (rotateCount === 0) return head;
+  let reversed = reverse(1, totalLen, head, "all");
+  let reversedStart = reverse(1, rotateCount, reversed, "start");
+  let reversedEnd = reverseEnd(rotateCount, totalLen, reversedStart);
+  return reversedEnd;
+};
+
+//Optimal and best solution
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function (head, k) {
+  if (!head || !head.next) return head;
+  let slow = head;
+  let fast = head;
+
+  let length = 0;
+  let curr = head;
+  while (curr) {
+    curr = curr.next;
+    length++;
+  }
+
+  k = k % length;
+
+  for (let i = 0; i < k; i++) {
+    fast = fast.next;
+  }
+
+  while (fast && fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+
+  fast.next = head;
+  head = slow.next;
+  slow.next = null;
+
+  return head;
 };
