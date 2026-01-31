294a1c2b83e3ab33ee4379d347a046c7988a710a
Kabilanp4 <kabilanperumal6@gmail.com>
2026-01-31 23:02:58 +0530

    leetcode #1207

---
+++ TopInterview150_and_75/Hashmap/uniqueOccurrences #1207.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function (arr) {
+  let map = {};
+  let set = new Set(arr);
+  let resultSet = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    let val = map[arr[i]] ? map[arr[i]] + 1 : 1;
+    map[arr[i]] = val;
+  }
+  for (const val of set) {
+    if (!resultSet.has(map[val])) {
+      resultSet.add(map[val]);
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
