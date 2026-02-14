03ede194785178542e129cee8768c2ced7b76fca
Kabilanp4 <kabilanperumal6@gmail.com>
2026-02-14 22:07:59 +0530

    Leetcode #735

--- TopInterview150_and_75/Stack/asteroidCollision#735.js
+++ TopInterview150_and_75/Stack/asteroidCollision#735.js
@@ -4,30 +4,37 @@
  */
 var asteroidCollision = function (asteroids) {
   if (!asteroids || !asteroids.length) return [];
-  let arr = [];
+  let stack = [];
   for (let i = 0; i < asteroids.length; i++) {
-    if (asteroids[i] < 0) {
-      let len = arr.length - 1;
-      console.log("arr len", len);
-      if (arr[len] + asteroids[i] === 0) {
-        arr.pop();
-      } else {
-        while (asteroids[i] < 0) {
-          arr.push(asteroids[i]);
-        }
-        while (arr[len] + asteroids[i] < 0) {
-          arr.pop();
-          len--;
-        }
-        if (!arr.length) arr.push(asteroids[i]);
-        console.log("arr", arr);
-      }
-    } else {
-      arr.push(asteroids[i]);
+    let stackLen = stack.length;
+    if (!stackLen) stack.push(asteroids[i]);
+    if (stack[stackLen - 1] < 0 && asteroids[i] < 0) stack.push(asteroids[i]);
+    if (stack[stackLen - 1] < asteroids[i]) stack.push(asteroids[i]);
+    // console.log("stack", stack);
+    if (stack[stackLen - 1] + asteroids[i] === 0) {
+      stack.pop();
+      continue;
     }
+
+    // stack = [-2,-2,1], arr = [-2,-2,1,-2]
+    while (
+      stack[stackLen - 1] > asteroids[i] &&
+      stack[stackLen - 1] + asteroids[i] < 0
+    ) {
+      stack.pop();
+      stackLen--;
+      // console.log("latest stack", stack);
+    }
+    if (!stack.length || (stack[stackLen - 1] < 0 && asteroids[i] < 0)) {
+      console.log("before stack", stack);
+      stack.push(asteroids[i]);
+      console.log("after stack", stack);
+    } // tbd
   }
-  console.log("arr", arr);
-  return arr;
+  return stack;
 };
 
-// Unfinished
+const output = asteroidCollision([-2, -2, 1, -2]);
+console.log("output", output);
+
+// I will resolve tomorrow.
